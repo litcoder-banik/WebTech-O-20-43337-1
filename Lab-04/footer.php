cd1f2ac048a34894b0bd19b758ab1ec7179d94dd
litcoder-banik <85700534+litcoder-banik@users.noreply.github.com>
2022-03-12 21:48:55 +0600

    Add files via upload

---
+++ Lab-04/footer.php
@@ -0,0 +1,7 @@
+<div style="width:100%; height:100px; border:1px solid #000;">
+
+	<?php
+		echo "<p> Copyright &copy; 2020-".date("Y")." Group_01</p>";
+	?>
+
+</div>
---
+++ Lab-04/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<div style="width:auto; height:100px; border:1px solid #000;">
+		<img src="ezlogo.png" height="80px" width="80px" style="margin-left: 10px; margin-top: 10px;">
+		
+		<div style="float: right; margin-top: 40px; margin-right: 10px;">
+			<a href="publichome.php">Home</a> |
+			<a href="login.php">Login</a> |
+			<a href="registration.php">Registration</a>
+		</div>		
+	</div>
+</body>
+</html>
+
